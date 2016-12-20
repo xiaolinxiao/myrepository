96ee4e41a80ebf53f078a35048892bc13f2162d2
xiaolinxiao <1057705307@qq.com>
2016-12-20 09:54:44 +0800

    第一个工作空间
    添加button

--- diebaoguan/app/src/main/java/com/newland/diebaoguan/MainActivity.java
+++ diebaoguan/app/src/main/java/com/newland/diebaoguan/MainActivity.java
@@ -2,6 +2,9 @@ package com.newland.diebaoguan;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
 
@@ -9,5 +12,12 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button btn = (Button) findViewById(R.id.button);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "点击了button",0).show();
+            }
+        });
     }
 }
